fb43fd1232a2874d1c2048583da9f051f577cc16
joydip-git <34733254+joydip-git@users.noreply.github.com>
2021-05-07 12:11:56 +0530

    json done

--- code/day-5/product-management-system/scripts/product-data.js
+++ code/day-5/product-management-system/scripts/product-data.js
@@ -12,13 +12,53 @@ function getProducts() {
     reqeust.onreadystatechange = function () {
         if (reqeust.readyState == 4 && reqeust.status == 200) {
             console.log(reqeust.responseText)
-        } else {
-            console.log("Ready State: " + reqeust.readyState)
+            console.log(JSON.parse(reqeust.responseText))
         }
+        //else {
+        //     console.log("Ready State: " + reqeust.readyState)           
+        // }
     }
 
     reqeust.open('GET', 'http://127.0.0.1:8081/productservice', true)
     reqeust.send();
 }
-
+//debugger
+/*
 getProducts()
+var jsonParameters =
+{
+    "x": 10
+}
+var jsData = [{
+    name: "Joydip",
+    id: 20,
+    isTrainer: true,
+    address: {
+        city: "Bangalore",
+        state: "KA",
+        pin: 560101
+    },
+    subject: ["HTML", "CSS", "JS"],
+    salary: 1234.56
+}, {
+    name: "Joydip",
+    id: 20,
+    isTrainer: true,
+    address: {
+        city: "Bangalore",
+        state: "KA",
+        pin: 560101
+    },
+    subject: ["HTML", "CSS", "JS"],
+    salary: 1234.56
+}];
+
+var jsonData = JSON.stringify(jsData);
+console.log(jsonData)
+var data = JSON.parse(jsonData)
+console.log(data)
+
+var jsonStringifiedObj = '[{"name":"anil","id":20}]';
+var jsObj = JSON.parse(jsonStringifiedObj);
+console.log(jsObj)
+*/
