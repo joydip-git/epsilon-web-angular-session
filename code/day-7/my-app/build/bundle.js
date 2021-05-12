9b995e77d0bdd01c99ae373c2a434e59979ac99f
joydip-git <34733254+joydip-git@users.noreply.github.com>
2021-05-12 11:28:26 +0530

    update-2

--- code/day-7/my-app/build/bundle.js
+++ code/day-7/my-app/build/bundle.js
@@ -25,7 +25,17 @@ eval("var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPAC
   \*************************/
 /***/ ((module, exports, __webpack_require__) => {
 
-eval("var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [__webpack_require__(/*! ./calculation */ \"./source/calculation.js\")], __WEBPACK_AMD_DEFINE_RESULT__ = (function (_calculation) {\n  \"use strict\";\n\n  console.log('running in browser runtime....');\n  console.log((0, _calculation.add)(10, 20));\n  console.log((0, _calculation.subtract)(10, 5));\n  console.log((0, _calculation.multiply)(10, 5));\n}).apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__),\n\t\t__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));\n\n//# sourceURL=webpack://my-app/./source/index.js?");
+eval("var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [__webpack_require__(/*! ./calculation */ \"./source/calculation.js\"), __webpack_require__(/*! ./person */ \"./source/person.js\")], __WEBPACK_AMD_DEFINE_RESULT__ = (function (_calculation, _person) {\n  \"use strict\";\n\n  // console.log('running in browser runtime....')\n  // console.log(add(10, 20))\n  // console.log(subtract(10, 5))\n  // console.log(multiply(10, 5))\n  function display() {\n    document.getElementById('addSpan').innerText = (0, _calculation.add)(10, 20);\n    document.getElementById('subSpan').innerText = (0, _calculation.subtract)(10, 20);\n    document.getElementById('multiSpan').innerText = (0, _calculation.multiply)(10, 20);\n    var anilPerson = new _person.person('anil', 101, 200000); //console.log(anilPerson)\n\n    document.getElementById('nameSpan').innerText = anilPerson.personName;\n    document.getElementById('idSpan').innerText = anilPerson.personId;\n    document.getElementById('salarySpan').innerText = anilPerson.personSalary;\n  }\n}).apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__),\n\t\t__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));\n\n//# sourceURL=webpack://my-app/./source/index.js?");
+
+/***/ }),
+
+/***/ "./source/person.js":
+/*!**************************!*\
+  !*** ./source/person.js ***!
+  \**************************/
+/***/ ((module, exports) => {
+
+eval("var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [exports], __WEBPACK_AMD_DEFINE_RESULT__ = (function (_exports) {\n  \"use strict\";\n\n  Object.defineProperty(_exports, \"__esModule\", {\n    value: true\n  });\n  exports.person = person;\n\n  function person(name, id, salary) {\n    this.personName = name;\n    this.personId = id;\n    this.personSalary = salary;\n\n    this.show = function () {\n      return this.personName + ', ' + this.personId + ', ' + this.personSalary;\n    };\n  }\n}).apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__),\n\t\t__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));\n\n//# sourceURL=webpack://my-app/./source/person.js?");
 
 /***/ })
 
--- code/day-7/my-app/source/index.js
+++ code/day-7/my-app/source/index.js
@@ -1,3 +1,4 @@
+//contain code to load the app in the index.html file
 import { add, subtract, multiply } from './calculation'
 import { person } from './person'
 console.log('running in browser runtime....')
@@ -6,5 +7,21 @@ console.log(subtract(10, 5))
 console.log(multiply(10, 5))
 
 const anilPerson = new person('anil', 101, 200000);
-console.log(anilPerson)
+console.log(anilPerson.show())
+
+// function display() {
+//     document.getElementById('addSpan').innerText = add(10, 20);
+//     document.getElementById('subSpan').innerText = subtract(10, 20);
+//     document.getElementById('multiSpan').innerText = multiply(10, 20);
+
+//     const anilPerson = new person('anil', 101, 200000);
+//     //console.log(anilPerson)
+//     document.getElementById('nameSpan').innerText = anilPerson.personName;
+//     document.getElementById('idSpan').innerText = anilPerson.personId;
+//     document.getElementById('salarySpan').innerText = anilPerson.personSalary;
+// }
+
+// window.onload = display;
+
+
 
